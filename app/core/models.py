ad6c7a75edc286c175e524594b684a49a4f9f8f6
DanJorna <>
2022-07-23 14:33:03 +0100

    create serializers

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
-    PermissionsMixin
+    PermissionsMixin,
 )
 
 
@@ -28,7 +28,7 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
---
+++ app/user/views.py
@@ -0,0 +1,6 @@
+from rest_framework import generics
+from user.serializers import UserSerializers
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializers
